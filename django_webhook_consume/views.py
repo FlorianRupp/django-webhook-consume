11d5f3be281c008a1b2bf08108072e43103cde7d
Florian Rupp <florian.rupp95@gmail.com>
2021-03-24 14:22:04 +0100

    Updated README.md.

--- django_webhook_consume/views.py
+++ django_webhook_consume/views.py
@@ -11,6 +11,11 @@ from django_webhook_consume.helper import check_hash, check_branch
 @http.require_POST
 @csrf.csrf_exempt
 def consume_web_hook(request):
+    """
+    Trigger on configured web hook. If called the hash of a given secret and received payload ist built. This hash is
+    compared with received control hash. If hashes match it is checked if the hook matched the branch.
+    If so the configured script is executed.
+    """
     payload = request.body
     if check_hash(os.environ[s.SECRET_KEY_NAME], payload, request.META[s.GITHUB_SECURE_HEADER]) is True:
         if check_branch(json.loads(payload), s.BRANCH):
